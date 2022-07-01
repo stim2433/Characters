71be18ed612e85a96f6dab9ab260de941a7f16b0
stimLite <gross2433@gmail.com>
2022-07-01 20:35:48 +0300

    Initial Commit

---
+++ Characters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Characters
+//
+//  Created by stimLite on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
